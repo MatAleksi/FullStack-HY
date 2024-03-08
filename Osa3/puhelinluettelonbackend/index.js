eeb89c0db5c0dcc1984ff6d9331d721990bf1ea3
MatAleksi <mattila.aleksi@hotmail.com>
2024-03-08 12:38:07 +0200

    static frontend version for backend

--- Osa3/puhelinluettelonbackend/index.js
+++ Osa3/puhelinluettelonbackend/index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 morgan.token("content", (request) => {
     return request.method === "POST" ? JSON.stringify(request.body) : " ";
