81701ab7d601ce3afcca5a0fb7548846f9e37118
MatAleksi <mattila.aleksi@hotmail.com>
2025-04-03 16:46:06 +0300

    4.15

---
+++ Osa4/bloglist/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    name: String,
+    passwordHash: String,
+    id: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.passwordHash
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model( 'User', userSchema )
