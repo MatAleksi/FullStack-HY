ae69baa8132f78b3883ce7d033ca78312eac6bbe
MatAleksi <mattila.aleksi@hotmail.com>
2025-04-28 17:51:43 +0300

    5.14-5.15

--- Osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -10,16 +10,47 @@ describe('<Blog />', () => {
     author: 'Test Author',
     url: 'http://testurl.com',
     likes: 0,
+    user:{
+      username: 'testuser',
+      name: 'Test User',
+    }
   }
+
   const user = {
     username: 'testuser',
     name: 'Test User',
   }
+
   const mockHandlerLike = vi.fn()
   const mockHandlerRemove = vi.fn()
 
-  test('title is rendered', () => {
-    render(<Blog blog={blog} user={user} handleLike={mockHandlerLike} handleRemove={mockHandlerRemove} />)
+  test('Title is rendered', () => {
+    render(<Blog blog={blog} user={user} addLike={mockHandlerLike} removeBlog={mockHandlerRemove} />)
     expect(screen.getByText('Test Title, Test Author')).toBeDefined()
+    expect(screen.queryByText('http://testurl.com')).toBeNull()
+    expect(screen.queryByText('0 likes')).toBeNull()
+  })
+
+  test('Url and likes are rendered when the view button is clicked', async () => {
+    const buttonClick = userEvent.setup()
+    render(<Blog blog={blog} user={user} addLike={mockHandlerLike} removeBlog={mockHandlerRemove} />)
+    const button = screen.getByText('view')
+    await buttonClick.click(button)
+
+    expect(screen.getByText('http://testurl.com')).toBeDefined()
+    expect(screen.getByText('0 likes')).toBeDefined()
+  })
+
+  test('Like button adds likes', async () => {
+    const buttonClick = userEvent.setup()
+    render(<Blog blog={blog} user={user} addLike={mockHandlerLike} removeBlog={mockHandlerRemove} />)
+    const button = screen.getByText('view')
+    await buttonClick.click(button)
+
+    const likeButton = screen.getByText('like')
+    await buttonClick.click(likeButton)
+    await buttonClick.click(likeButton)
+
+    expect(screen.queryByText('2 likes')).toBeDefined()
   })
 })
