3256e87cce60298d00d6bf590f3277d6466a4747
MatAleksi <mattila.aleksi@hotmail.com>
2025-05-30 22:35:32 +0300

    7.4

--- Osa7/routed-anecdotes/src/App.jsx
+++ Osa7/routed-anecdotes/src/App.jsx
@@ -4,6 +4,7 @@ import {
   Routes, Route, Link, useParams,
   useNavigate
 } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = ({anecdotes, addNew, notification}) => {
   const padding = {
@@ -76,17 +77,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
   const navigate = useNavigate()
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -96,19 +97,25 @@ const CreateNew = (props) => {
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
+        <input 
+          type={content.type}
+          value={content.value}
+          onChange={content.onChange}
+        />
+        <br/>
+        <input
+          type={author.type}
+          value={author.value}
+          onChange={author.onChange}
+        />
+        <br/>
+        <input
+          type={info.type}
+          value={info.value}
+          onChange={info.onChange}
+        />
+        <br/>
+        <button type="submit">create</button>
       </form>
     </div>
   )
